745075e76616ac34972452cea4b3417551ff5aca
osully1 <owensullivan482@gmail.com>
2022-02-15 19:12:01 -0500

    m

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-
+//
 function App() {
   return (
     <div className="App">
