d8244084b475447112e7e1d27673c72ec14d55d1
osully1 <owensullivan482@gmail.com>
2022-02-28 14:38:34 -0500

    cleaning up

--- src/components/ScoreBoard/ScoreBoard.js
+++ src/components/ScoreBoard/ScoreBoard.js
@@ -1,12 +1,10 @@
 import styles from './ScoreBoard.module.css'
 import ScoreCategory from '../ScoreCategory/ScoreCategory'
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { selectScore } from '../../store/scoreSlice';
-import { selectCount } from '../../store/countSlice';
 
 const ScoreBoard = (props) => {
     const score = useSelector(selectScore);
-    const count = useSelector(selectCount);
 
     return Object.keys(score).map((category, idx) => {
         const scoreValue = score[`${category}`]
